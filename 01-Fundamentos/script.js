69b07f34149076c1a54f0ef448cf93b0504929a3
Hector Adrian Benavidez <adrianbenavidez119@gmail.com>
2023-11-26 07:25:28 -0300

    Version 1.0

---
+++ 01-Fundamentos/script.js
@@ -0,0 +1,16 @@
+//values, variables and data types 
+
+let firstName = "Hector"
+let lastName = "Benavidez"
+let age = 39
+let enBusquedaLaboral = true
+let children;
+
+console.log(firstName);
+console.log(typeof(firstName));
+console.log(typeof(children));
+console.log(typeof(age));
+console.log(typeof(enBusquedaLaboral));
+
+
+//basic operators
